d4d4f4de75d802a94c3e7ccfd5912d9408e1eba9
mac <waihanko@tamronnetwork.com>
2022-01-12 13:05:17 +0630

    - Fun Feed Detail

---
+++ lib/src/data_source/network/wisdom_api.g.dart
@@ -0,0 +1,62 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'wisdom_api.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+class _WisdomAPI implements WisdomAPI {
+  _WisdomAPI(this._dio, {this.baseUrl}) {
+    baseUrl ??= 'http://128.199.128.58/mobile';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<AppVersionVo> checkAppVersion() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<AppVersionVo>(
+            Options(method: 'GET', headers: _headers, extra: _extra)
+                .compose(_dio.options, '/check-version/wisdom-android',
+                    queryParameters: queryParameters, data: _data)
+                .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = AppVersionVo.fromJson(_result.data!);
+    return value;
+  }
+
+  @override
+  Future<PostListVo> getFunList() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<PostListVo>(
+            Options(method: 'GET', headers: _headers, extra: _extra)
+                .compose(_dio.options, '/wisdom/posts',
+                    queryParameters: queryParameters, data: _data)
+                .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = PostListVo.fromJson(_result.data!);
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
